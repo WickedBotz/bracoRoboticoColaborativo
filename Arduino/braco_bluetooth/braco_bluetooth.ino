7c554d09640f553a3c18a2d073061f0e76e2b80a
igorfachini <igor.fachini@catolicasc.org.br>
2018-10-04 12:53:57 -0300

    Arduino, CAB methods for remote control

--- Arduino/braco_bluetooth/braco_bluetooth.ino
+++ Arduino/braco_bluetooth/braco_bluetooth.ino
@@ -10,13 +10,22 @@ SoftwareSerial serial1(11, 10); // RX, TX
 
 Servo servo_a, servo_b, servo_c, servo_d;
 
-String data;
+String data = "0";
 int a = 15, b = 90, c = 90, d = 90, time_servo = 15;
 
-void setup() {
+// MAPEAMENTO DIRECOES BRACO
+//A FRENTE tras - cervo esquerda  < pra tras > pra frente
+//b garra < abre > fecha
+//c cima baixo - cervo direita < pra frente > pra tras
+//d direita esquerda - cervo baixo < pra esquerda > pra direita
+
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
   serial1.begin(9600);
-  serial1.println("Starting arm...");
+
+  Serial.println("Starting arm...");
 
   servo_a.attach(SERVO_A);
   servo_b.attach(SERVO_B);
@@ -29,79 +38,183 @@ void setup() {
   servo_d.write(d);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if (serial1.available()) {
-    data = serial1.readString();
-    serial1.println(data);
-    char option = data.charAt(0);
-    data.remove(0, 1);
-    serial1.println(data);
-    int var = data.toInt();
-
-    switch (option) {
-      case 'a':
-        var = constrain(var, 10, 120);
-        serial1.print("SERVO_A = ");
-        serial1.println(var);
-        while (a != var) {
-          if (a > var) {
-            a--;
-          }
-          if (a < var) {
-            a++;
-          }
-          servo_a.write(a);
-          delay(time_servo);
-        }
-        break;
-      case 'b':
-        var = constrain(var, 70, 110);
-        serial1.print("SERVO_B = ");
-        serial1.println(var);
-        while (b != var) {
-          if (b > var) {
-            b--;
-          }
-          if (b < var) {
-            b++;
-          }
-          servo_b.write(b);
-          delay(time_servo);
-        }
-        break;
-      case 'c':
-        var = constrain(var, 0, 140);
-        serial1.print("SERVO_C = ");
-        serial1.println(var);
-        while (c != var) {
-          if (c > var) {
-            c--;
-          }
-          if (c < var) {
-            c++;
-          }
-          servo_c.write(c);
-          delay(time_servo);
-        }
-        break;
-      case 'd':
-        var = constrain(var, 10, 170);
-        serial1.print("SERVO_D = ");
-        serial1.println(var);
-        while (d != var) {
-          if (d > var) {
-            d--;
-          }
-          if (d < var) {
-            d++;
-          }
-          servo_d.write(d);
-          delay(time_servo);
-        }
-        break;
-    }
-  }
-
+  app_arm_control();
 }
 
+// Para controle manual, recebe a letra do servo e a posicao
+// Ex: a20
+void manual_arm_control()
+{
+  if (Serial.available())
+  {
+    data = Serial.readString();
+    char option = data.charAt(0);
+    data.remove(0, 1);
+    int var = data.toInt();
+
+    switch (option)
+    {
+    case 'a':
+      var = constrain(var, 10, 120);
+      Serial.print("SERVO_A = ");
+      Serial.println(var);
+      while (a != var)
+      {
+        if (a > var)
+        {
+          a--;
+        }
+        if (a < var)
+        {
+          a++;
+        }
+        servo_a.write(a);
+        delay(time_servo);
+      }
+      break;
+    case 'b':
+      var = constrain(var, 70, 110);
+      Serial.print("SERVO_B = ");
+      Serial.println(var);
+      while (b != var)
+      {
+        if (b > var)
+        {
+          b--;
+        }
+        if (b < var)
+        {
+          b++;
+        }
+        servo_b.write(b);
+        delay(time_servo);
+      }
+      break;
+    case 'c':
+      var = constrain(var, 0, 140);
+      Serial.print("SERVO_C = ");
+      Serial.println(var);
+      while (c != var)
+      {
+        if (c > var)
+        {
+          c--;
+        }
+        if (c < var)
+        {
+          c++;
+        }
+        servo_c.write(c);
+        delay(time_servo);
+      }
+      break;
+    case 'd':
+      var = constrain(var, 10, 170);
+      Serial.print("SERVO_D = ");
+      Serial.println(var);
+      while (d != var)
+      {
+        if (d > var)
+        {
+          d--;
+        }
+        if (d < var)
+        {
+          d++;
+        }
+        servo_d.write(d);
+        delay(time_servo);
+      }
+      break;
+    }
+  }
+}
+
+//Para ser controlodo pelo app
+void app_arm_control()
+{
+  if (serial1.available() > 0)
+  {
+    data = serial1.readStringUntil(';');
+  }
+  char direction = data.charAt(0);
+  switch (direction)
+  {
+  case '8':
+    if (a < 120)
+    {
+      a++;
+      servo_a.write(a);
+      delay(time_servo);
+    }
+
+    break;
+  case '4':
+    if (d < 170)
+    {
+      d++;
+      servo_d.write(d);
+      delay(time_servo);
+    }
+
+    break;
+  case '6':
+    if (d > 10)
+    {
+      d--;
+      servo_d.write(d);
+      delay(time_servo);
+    }
+
+    break;
+  case '2':
+    if (a > 10)
+    {
+      a--;
+      servo_a.write(a);
+      delay(time_servo);
+    }
+
+    break;
+  case 'x':
+    if (c < 140)
+    {
+      c++;
+      servo_c.write(c);
+      delay(time_servo);
+    }
+
+    break;
+  case 'a':
+    if (c > 0)
+    {
+      c--;
+      servo_c.write(c);
+      delay(time_servo);
+    }
+
+    break;
+
+  case 'y':
+    if (b > 70)
+    {
+      b--;
+      servo_b.write(b);
+      delay(time_servo);
+    }
+
+    break;
+  case 'z':
+    if (b < 110)
+    {
+      b++;
+      servo_b.write(b);
+      delay(time_servo);
+    }
+
+    break;
+  }
+}
